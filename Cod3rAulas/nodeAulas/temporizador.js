7dda1dd24fa8dfef54b7bf72a0c21cb34dbd0e31
igor98santos <85197556+igor98santos@users.noreply.github.com>
2022-09-27 14:48:31 -0300

    Fim de Dom

--- Cod3rAulas/nodeAulas/temporizador.js
+++ Cod3rAulas/nodeAulas/temporizador.js
@@ -1,3 +1,4 @@
+
 const schedule = require('node-schedule')
 
 const tarefa1 = schedule.scheduleJob('*/5 * 12 * * 2', function () {
---
+++ Cod3rAulas/nodeAulas/teste.js
@@ -0,0 +1,11 @@
+var par
+var i = 0
+var valor = 100
+while(i <= valor){
+    if(i % 2 ==  0){
+        console.log('é par: '+ i)
+    }else{
+        console.log('Impares: '+i)
+    }
+    i = i + 1
+}
